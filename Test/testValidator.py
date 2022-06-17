63e5a9141bdd7f704949b87fbda0b77d39fff823
David <davidcfox96@gmail.com>
2022-06-17 12:54:57 +0100

    Adding outward code validation

--- Test/testValidator.py
+++ Test/testValidator.py
@@ -5,49 +5,49 @@ import source.postCodeValidator as val
 class ValidatorTest(unittest.TestCase):
 
     def testValidCase(self):
-        output = val.validatePostCode("abcdefgh")
+        output = val.validatePostCode("EC1A 1BB")
         print(output)
         self.assertEqual(output["errorCode"], 0)
         self.assertEqual(len(output["errors"]), 0)
 
     def testInvalidCase_DigitInAreaCode(self):
-        output = val.validatePostCode("abcd")
+        output = val.validatePostCode("1C1A 1BB")
         self.assertEqual(output["errorCode"], 1)
         self.assertEqual(len(output["errors"]), 1)
         self.assertEqual(output["errors"][0], "outward code Invalid")
 
     def testInvalidCase_NoDigitInDistrictCode(self):
-        output = val.validatePostCode("abcd")
+        output = val.validatePostCode("ECAA 1BB")
         self.assertEqual(output["errorCode"], 1)
         self.assertEqual(len(output["errors"]), 1)
         self.assertEqual(output["errors"][0], "outward code Invalid")
 
     def testInvalidCase_LetterInPostcodeSector(self):
-        output = val.validatePostCode("abcd")
+        output = val.validatePostCode("EC1A 1BB")
         self.assertEqual(output["errorCode"], 1)
         self.assertEqual(len(output["errors"]), 1)
         self.assertEqual(output["errors"][0], "inward code Invalid")
 
     def testInvalidCase_NumberInPostcodeUnit(self):
-        output = val.validatePostCode("abcd")
+        output = val.validatePostCode("EC1A 1BB")
         self.assertEqual(output["errorCode"], 1)
         self.assertEqual(len(output["errors"]), 1)
         self.assertEqual(output["errors"][0], "inward code Invalid")
 
     def testInvalidCase_MultipleErrors(self):
-        output = val.validatePostCode("abcd")
+        output = val.validatePostCode("EC1A 1BB")
         self.assertEqual(output["errorCode"], 1)
         self.assertEqual(len(output["errors"]), 1)
         self.assertEqual(output["errors"][0], "inward code Invalid")
 
     def testInvalidCase_TooLong(self):
-        output = val.validatePostCode("ABABABABA")
+        output = val.validatePostCode("EC1A 1BBA")
         self.assertEqual(output["errorCode"], 1)
-        self.assertEqual(len(output["errors"]), 1)
+        self.assertEqual(len(output["errors"]), 2)
         self.assertEqual(output["errors"][0], "given postcode is too long")
 
     def testInvalidCase_TooShort(self):
-        output = val.validatePostCode("abcd")
+        output = val.validatePostCode("EC1A ")
         self.assertEqual(output["errorCode"], 1)
-        self.assertEqual(len(output["errors"]), 1)
+        self.assertEqual(len(output["errors"]), 2)
         self.assertEqual(output["errors"][0], "given postcode is too Short")
--- source/postCodeValidator.py
+++ source/postCodeValidator.py
@@ -1,3 +1,10 @@
+# Validations based on https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom#Formatting
+# as of 17/06/22
+import re
+
+outwardCode = '^[A-Z]{1,2}[0-9][A-Z0-9]?'
+
+
 def validateLength(postcode, response):
     codeLength = len(postcode)
     minlength = 6
@@ -9,9 +16,17 @@ def validateLength(postcode, response):
             response["errors"].append("given postcode is too Short")
 
 
+def validateOutwardCode(postcode, response):
+    matches = re.search(outwardCode, postcode)
+    if matches is None:
+        response["errors"].append("outward code Invalid")
+    return postcode
+
+
 def validatePostCode(postcode):
     response = buildResponse()
     validateLength(postcode, response)
+    validateOutwardCode(postcode, response)
 
     if len(response["errors"]) > 0:
         response["errorCode"] = 1
